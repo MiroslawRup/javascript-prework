1a3e822978ededfeb49648a10ff52fac1d7f82ea
Mirosław Rup <chrupek999@gmail.com>
2021-01-26 19:06:15 +0100

    Correct [let] to [const] and implementation anonymous function

--- js/script.js
+++ js/script.js
@@ -1,64 +1,64 @@
 {
 
 	/* get info which button is clicked by user and give it to the function playGame */
-	document.getElementById('play-rock').addEventListener('click', function(){
+	document.getElementById('play-rock').addEventListener('click', function() {
 		playGame(1);
 	});
-	document.getElementById('play-paper').addEventListener('click', function(){
+	document.getElementById('play-paper').addEventListener('click', function() {
 		playGame(2);
 	});
-	document.getElementById('play-scissors').addEventListener('click', function(){
+	document.getElementById('play-scissors').addEventListener('click', function() {
 		playGame(3);
 	});
 
-function playGame(playerInput){
+	const playGame = function(playerInput) {
 
-	/* function to clean up previous displayed game results */
-	clearMessages();
-
-	/* change user number to game word in getMoveName function */
-	let playerMove = getMoveName(playerInput);
-
-	/* draw computer operation */
-	let randomNumber = Math.floor(Math.random() * 3 + 1);
-	/* change computer drawn number to game word in getMoveName function*/
-	let computerMove = getMoveName(randomNumber);
-
-	function getMoveName(argMoveId){
-		if(argMoveId == 1){
-			return 'kamień';
-		} else if(argMoveId == 2){
-			return 'papier';
-		} else if(argMoveId == 3){
-			return 'nożyce';
+		/* change user number to game word */
+		const getMoveName = function(argMoveId) {
+			if(argMoveId == 1) {
+				return 'kamień';
+			} else if(argMoveId == 2) {
+				return 'papier';
+			} else if(argMoveId == 3) {
+				return 'nożyce';
+			}
 		}
-	}
 
-	/* display computer and user selection */
-	printMessage('Ruch komputera to: ' + computerMove);
-	printMessage('Twój ruch to: ' + playerMove);
+		const playerMove = getMoveName(playerInput);
 
-	/* determine game result */
-		function displayResult(argComputerMove, argPlayerMove) {
+		/* draw computer operation */
+		const randomNumber = Math.floor(Math.random() * 3 + 1);
+		/* change computer drawn number to game word in getMoveName function*/
+		const computerMove = getMoveName(randomNumber);
+
+		/* function to clean up previous displayed game results */
+		clearMessages();
+
+		/* display computer and user selection */
+		printMessage('Ruch komputera to: ' + computerMove);
+		printMessage('Twój ruch to: ' + playerMove);
+
+		/* determine game result */
+		const displayResult = function(argComputerMove, argPlayerMove) {
 			if (argComputerMove == argPlayerMove) {
 				printMessage('Remis!');
-			} else if (argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+			} else if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
 				printMessage('Ty wygrywasz!');
-			} else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+			} else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
 				printMessage('Ty wygrywasz!');
-			} else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+			} else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
 				printMessage('Ty wygrywasz!');
-			} else if (argPlayerMove == 'kamień' && argComputerMove == 'papier'){
+			} else if (argPlayerMove == 'kamień' && argComputerMove == 'papier') {
 				printMessage('Komputer wygrywa!');
-			} else if (argPlayerMove == 'nożyce' && argComputerMove == 'kamień'){
+			} else if (argPlayerMove == 'nożyce' && argComputerMove == 'kamień') {
 				printMessage('Komputer wygrywa!');
-			} else if (argPlayerMove == 'papier' && argComputerMove == 'nożyce'){
+			} else if (argPlayerMove == 'papier' && argComputerMove == 'nożyce') {
 				printMessage('Komputer wygrywa!');
 			}
 		}
+
 		/* display game result */
 		displayResult(computerMove, playerMove);
-
 	}
 
 }
